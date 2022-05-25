7b1131289813a04578ddbcee0eee2db980a07827
vicevortex11 <58641673+vicevortex11-tech@users.noreply.github.com>
2022-05-25 17:51:02 +0300

    Updated Navigation bar & Footer.
    Reconstruction of the footer and navbar.

--- webapp/resources/views/inc/footer.blade copy.php
+++ webapp/resources/views/inc/footer.blade copy.php
@@ -11,32 +11,36 @@
 </style>
 
 <footer class="footer">
+    <div class="container">
+      <p class="text-muted">Place sticky footer content here.</p>
+    </div>
+
     <div class="container text-center">
         <hr />
       <div class="row">
         <div class="col-lg-12">
           <div class="col-md-3">
             <ul class="nav nav-pills nav-stacked">
-              <li><a href="/about">About us</a></li>
-              <li><a href="/posts">Blog</a></li>
+              <li><a href="#">About us</a></li>
+              <li><a href="#">Blog</a></li>
             </ul>
           </div>
           <div class="col-md-3">
             <ul class="nav nav-pills nav-stacked">
-              <li><a href="#">Broadcasts</a></li>
-              <li><a href="#">Advertisements</a></li>
+              <li><a href="#">Product for Mac</a></li>
+              <li><a href="#">Product for Windows</a></li>
             </ul>
           </div>
           <div class="col-md-3">
             <ul class="nav nav-pills nav-stacked">
-              <li><a href="#">Partners</a></li>
-              <li><a href="#">Jobs</a></li>          
+              <li><a href="#">Web analytics</a></li>
+              <li><a href="#">Presentations</a></li>          
             </ul>
           </div>
           <div class="col-md-3">
             <ul class="nav nav-pills nav-stacked">
-              <li><a href="#">Contacts</a></li>
-              <li><a href="/players">Players</a></li>
+              <li><a href="#">Product Help</a></li>
+              <li><a href="#">Developer API</a></li>
             </ul>
           </div>  
         </div>
@@ -45,7 +49,7 @@
         <div class="row">
             <div class="col-lg-12">
                 <ul class="nav nav-pills nav-justified">
-                    <li><a href="/">© 2022 Vortech inc.</a></li>
+                    <li><a href="/">© 2013 Company Name.</a></li>
                     <li><a href="#">Terms of Service</a></li>
                     <li><a href="#">Privacy</a></li>
                 </ul>
--- webapp/resources/views/inc/navbar.blade.php
+++ webapp/resources/views/inc/navbar.blade.php
@@ -1,24 +1,27 @@
-<nav class="navbar navbar-inverse">
-    <div class="container">
-      <div class="navbar-header">
-        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-          <span class="sr-only">Toggle navigation</span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-        <a class="navbar-brand" href="#">{{config('app.name','TPLIS')}}</a>
-      </div>
-      <div id="navbar" class="collapse navbar-collapse">
-        <ul class="nav navbar-nav">
-          <li><a href="/home">Home</a></li>
-          <li><a href="/table">Table</a></li>
-          <li><a href="/clubs">Clubs</a></li>
-          <li><a href="/fixtures">Fixtures</a></li>
-          <li><a href="/players">Players</a></li>
-          <li><a href="/posts">Blog</a></li>
-          <li><a href="/about">About</a></li>
-        </ul>
-      </div><!--/.nav-collapse -->
+<!-- Fixed navbar -->
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">{{config('app.name','TPLIS')}}</a>
     </div>
-  </nav>
+    <div id="navbar" class="navbar-collapse collapse">
+      <ul class="nav navbar-nav">
+        <li><a href="/home">Home</a></li>
+        <li><a href="/table">Table</a></li>
+        <li><a href="/clubs">Clubs</a></li>
+        <li><a href="/fixtures">Fixtures</a></li>
+        <li><a href="/players">Players</a></li>
+        <li><a href="/posts">Blog</a></li>
+        <li><a href="/about">About</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<br>
+<br>
